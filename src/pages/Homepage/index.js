deeae18300719d81f306220d6e5b7405a049b220
Colin Stringer <colinstringer2@gmail.com>
2020-05-12 16:25:16 +0200

    created the architecture for the pages

---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      Home page
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/pages/ViewPage/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+import React from 'react'
+
+function ViewPage() {
+  return (
+    <div>
+      View page
+    </div>
+  )
+}
+
+export default ViewPage
